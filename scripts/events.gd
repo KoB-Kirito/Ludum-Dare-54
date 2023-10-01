f588470d1d6c4d8b3f98c2b6cffb040b9c8f1330
Kirito <kilito@pm.me>
2023-10-01 18:14:12 +0200

    wip

--- scripts/events.gd
+++ scripts/events.gd
@@ -6,9 +6,11 @@ signal character_selected(character: Character, bed: Bed)
 signal food_collected(amount: int)
 signal water_collected(amount: int)
 signal wood_collected(amount: int)
+signal ammo_collected(amount: int)
 
 signal food_changed(value: int)
 signal water_changed(value: int)
 signal wood_changed(value: int)
+signal ammo_changed(value: int)
 
 signal show_text(text: String, color: Color)
